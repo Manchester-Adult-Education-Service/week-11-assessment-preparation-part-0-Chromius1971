b95a431c8abfcdcdc55a50c29041fafb80f329b1
Chromius1971 <798940@manadulted.ac.uk>
2025-12-04 12:25:01 +0000

    Completed Extension 2 and upgrades!

--- Ex1_assessment_prep.py
+++ Ex1_assessment_prep.py
@@ -448,88 +448,7 @@ choice = 0
 # 6. Change your exit section to: elif choice == "4":
 #
 # Write your code (modify existing code):
-while choice != "4":
-    print("1. Add task")
-    print("2. View all task")
-    print("3. Search for task")
-    print("4. Exit")
-    print()
-    choice = input("Select an option: ")
-    while choice != "1" and choice != "2" and choice != "3" and choice != "4":
-       print("ERROR: Invalid choice")
-       print()
-       print("1. Add task")
-       print("2. View all tasks")
-       print("3. Search for task")
-       print("4. Exit")
-       print()
-       choice = input("Select an option: ")
-       print()
-    if choice == "1":
-        print()
-        print(f"You selected option: {choice}")
-        print()
-        task_name = input("Enter task name: ")
-        while task_name == "":
-            print("ERROR - Task name CANNOT be blank!")
-            task_name = input("Enter task name: ")
-        priority = input("Enter task priority (High/Medium/Low): ")
-        while priority == "":
-            print("ERROR - Priority CANNOT be blank!")
-            priority = input("Enter task priority (High/Medium/Low): ")
-        print()
-        task = {"name": task_name, "priority": priority, "status": "Not started"}
-        tasks.append(task)
-        print("Task added sucessfully!")
-        print()
-    elif choice == "2":
-       print()
-       print(f"You selected option: {choice}")
-       print()
-       if len(tasks) == 0:
-           print("No tasks recorded yet")
-           print()
-       else:
-           print("All tasks:")
-           print()
-           for task in tasks:
-               print(f"Task: {task['name']}")
-               print(f"Priority: {task['priority']}")
-               print(f"Status: {task['status']}")
-               print("---------------------------------------")
-           print()
-    elif choice == "3":
-        print()
-        print(f"You selected option: {choice}")
-        print()
-        if len(tasks) == 0:
-            print("There are no tasks to search")
-        else:
-           search_name = input("Enter task name to search for: ")
-           found = False
-           for task in tasks:
-               if task["name"] == search_name:
-                   print()
-                   print("Task found:")
-                   print()
-                   print(f"Task: {task['name']}")
-                   print(f"Priority: {task['priority']}")
-                   print(f"Status: {task['status']}")
-                   print()
-                   found = True
-           if found == False:
-               print()
-               print("No matching tasks found")
-#                print()
-    elif choice == "4":
-        print()
-        print("Thank you for using the Task Manager")
-        print("Goodbye")
-        print()
-    
-    # print(f"You selected option: {choice}") I moved this to the first line after the choice made as it 
-    print()                                 # made more sense to have this message appear BEFORE any other
-                                            # informtion
+
 
 
 
@@ -584,7 +503,115 @@ while choice != "4":
 # 5. Update your exit to: elif choice == "5":
 #
 # Write your code (modify existing code):
-
+while choice != "5":
+    print("1. Add task")
+    print("2. View all task")
+    print("3. Search for task")
+    print("4. Statistics")
+    print("5. Exit")
+    print()
+    choice = input("Select an option: ")
+    while choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5":
+       print("ERROR: Invalid choice")
+       print()
+       print("1. Add task")
+       print("2. View all tasks")
+       print("3. Search for task")
+       print("4. Statistics")
+       print("5. Exit")
+       print()
+       choice = input("Select an option: ")
+       print()
+    if choice == "1":
+        print()
+        print(f"You selected option: {choice}")
+        print()
+        task_name = input("Enter task name: ")
+        while task_name == "":
+            print("ERROR - Task name CANNOT be blank!")
+            task_name = input("Enter task name: ")
+        priority = input("Enter task priority (High/Medium/Low): ")
+        while priority == "":
+            print("ERROR - Priority CANNOT be blank!")
+            priority = input("Enter task priority (High/Medium/Low): ")
+        print()
+        task = {"name": task_name.title(), "priority": priority.title(), "status": "Not started"} # Added .title() to
+        tasks.append(task)                         # task variable to ensure even if typed in lowercase by user the  
+        print("Task added sucessfully!")           # priorities are included in statistics otherwise they aren't counted
+        print()
+    elif choice == "2":
+       print()
+       print(f"You selected option: {choice}")
+       print()
+       if len(tasks) == 0:
+           print("No tasks recorded yet")
+           print()
+       else:
+           print("All tasks:")
+           print()
+           for task in tasks:
+               print(f"Task: {task['name']}")
+               print(f"Priority: {task['priority']}")
+               print(f"Status: {task['status']}")
+               print("---------------------------------------")
+           print()
+    elif choice == "3":
+        print()
+        print(f"You selected option: {choice}")
+        print()
+        if len(tasks) == 0:
+            print("There are no tasks to search")
+        else:
+           search_name = input("Enter task name to search for: ")
+           found = False
+           for task in tasks:
+               if task["name"] == search_name.title():  # Added .title() to search_name variable to ensure match with
+                   print()                              # stored tasks after adding .title() to task variable
+                   print("Task found:")
+                   print()
+                   print(f"Task: {task['name']}")
+                   print(f"Priority: {task['priority']}")
+                   print(f"Status: {task['status']}")
+                   print()
+                   found = True
+           if found == False:
+               print()
+               print("No matching tasks found!")
+               print()
+    elif choice == "4":
+        print()
+        print(f"You selected option: {choice}")
+        print()
+        if len(tasks) == 0:
+           print("No tasks to calculate statistics with!")
+           print()
+        else:
+            high_count = 0
+            medium_count = 0
+            low_count = 0
+            for task in tasks:
+                if task["priority"] == "High":
+                    high_count = high_count + 1
+                elif task["priority"] == "Medium":
+                    medium_count = medium_count + 1
+                elif task["priority"] == "Low":
+                    low_count = low_count + 1
+            print("Task Statistics:")
+            print(f"Total tasks: {len(tasks)}")
+            print(f"High priority: {high_count}")
+            print(f"Medium priority: {medium_count}")
+            print(f"Low priority: {low_count}")
+            print()
+        
+    elif choice == "5":
+        print()
+        print("Thank you for using the Task Manager")
+        print("Goodbye")
+        print()
+    
+    # print(f"You selected option: {choice}") I moved this to the first line after the choice made as it 
+    print()                                 # made more sense to have this message appear BEFORE any other
+                                            # informtion
 
 
 
